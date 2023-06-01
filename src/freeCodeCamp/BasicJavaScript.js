16b0fe10b7df4d7395df76906075a65cf9538c88
Andrey Shakurov <amshakurov@gmail.com>
2023-06-01 13:12:18 +0700

    Basic JavaScript:1-4

---
+++ src/freeCodeCamp/BasicJavaScript.js
@@ -0,0 +1,22 @@
+// Comment Your JavaScript Code
+
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+// Declare JavaScript Variables
+
+var myName;
+
+// Storing Values with the Assignment Operator
+
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+
+var a;
+a = 7;
+var b;
+b = a;
