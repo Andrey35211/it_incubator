39b2bc7feb3db0365d4aa8219fdd946eb0f89d18
Andrey Shakurov <amshakurov@gmail.com>
2023-06-01 20:01:09 +0700

    codeWars:3_tasks

---
+++ src/codeWars/kyu8.js
@@ -0,0 +1,25 @@
+// Function 1 - hello world
+
+const greet = () => {
+    return "hello world!";
+}
+
+// Convert boolean values to strings 'Yes' or 'No'.
+
+let bool = 0;
+
+function boolToWord( bool ){
+    if(bool == true) {
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
+
+// Sentence Smash
+
+let words = ['hello', 'world', 'this', 'is', 'great'];
+
+function smash (words) {
+    return words.join(' ')
+};
