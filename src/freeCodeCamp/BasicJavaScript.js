a25d9ffaf8aa13f3073dffbe4a3153c06df15375
Andrey Shakurov <amshakurov@gmail.com>
2023-06-02 21:03:03 +0700

    Bootstrap:17/31

--- src/freeCodeCamp/BasicJavaScript.js
+++ src/freeCodeCamp/BasicJavaScript.js
@@ -183,3 +183,54 @@ let lastNameLength = 0;
 const lastName = "Lovelace";
 
 lastNameLength = lastName.length;
+
+// Use Bracket Notation to Find the First Character in a String
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+
+// Understand String Immutability
+
+let myStr = "Jello World";
+
+myStr = "Hello World";
+
+// Use Bracket Notation to Find the Nth Character in a String
+
+const lastName = "Lovelace";
+
+const thirdLetterOfLastName = lastName[2];
+
+// Use Bracket Notation to Find the Last Character in a String
+
+const lastName = "Lovelace";
+
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+const lastName = "Lovelace";
+
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " !";
+
+// Store Multiple Values in one Variable using JavaScript Arrays
+
+const myArray = ["hello", 3];
+
+// Nest one Array within Another Array
+
+const myArray = [["Bulls", 23], ["White Sox", 45]];
+
+//Access Array Data with Indexes
+
