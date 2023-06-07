4fa3b656dcf2603241fd4d12799e2e82ea9722f8
Andrey35211 <serand3521@yandex.ru>
2023-06-07 13:41:29 +0700

    codeWar 8kyu: 3 tasks

--- src/codeWars/kyu8.js
+++ src/codeWars/kyu8.js
@@ -174,3 +174,11 @@ class Kata {
     }
 }
 
+// How much water do I need?
+
+function howMuchWater(water, load, clothes){
+    if(clothes > load * 2) return 'Too much clothes';
+    else if (load > clothes) return 'Not enough clothes';
+    else return +(water * 1.1 ** (clothes - load)).toFixed(2)
+}
+
