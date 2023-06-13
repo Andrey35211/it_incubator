7733470601745e05ae66369a68ef11e9c7360ae2
Andrey35211 <serand3521@yandex.ru>
2023-06-13 11:38:21 +0700

    freeCodeCamp React: 12/47

--- src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
+++ src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
@@ -181,3 +181,22 @@ class TypesOfFood extends React.Component {
         );
     }
 };
+
+// Render a Class Component to the DOM
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+                <Fruits />
+                <Vegetables />
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
