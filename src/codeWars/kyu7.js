909c77146aa163d35f74e58785b6f373696b62ea
Andrey35211 <serand3521@yandex.ru>
2023-06-13 13:58:23 +0700

    codeWar 7kuy: Sum of the first nth term of Series

--- src/codeWars/kyu7.js
+++ src/codeWars/kyu7.js
@@ -110,3 +110,17 @@ function flattenAndSort(array) {
     })
     return res.sort((a,b) => a-b)
 }
+
+// Sum of the first nth term of Series
+
+function SeriesSum(n)
+{
+    if(!n) return '0.00'
+    let sum = 1;
+    let j = 4;
+    for(let i =1; i < n; i++) {
+        sum += 1 / j;
+        j += 3
+    }
+    return sum.toFixed(2);
+}
