0a9935128ba3c955b733f1ac64ee93d8dd358858
Andrey35211 <serand3521@yandex.ru>
2023-06-13 11:31:37 +0700

    codeWar 6kyu

--- src/codeWars/6kyu.js
+++ src/codeWars/6kyu.js
@@ -3,3 +3,17 @@
 function arraysSimilar(arr1, arr2) {
     return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort())
 }
+
+// Reverse every other word in the string
+
+function reverse(str){
+    return str
+        .trim()
+        .split(' ')
+        .map((el, i) => {
+            return i % 2
+                ? el.split('').reverse().join('')
+                : el;
+        })
+        .join(' ')
+}
