4c1fdf2a4c8430eb5dbe9b481dd3a88d7d76e6a0
Andrey35211 <serand3521@yandex.ru>
2023-06-14 22:50:37 +0700

    freeCodeCamp React: 16/47

--- src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
+++ src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
@@ -240,3 +240,39 @@ class Calendar extends React.Component {
         );
     }
 };
+
+// Pass an Array as Props
+
+const List = (props) => {
+    { /* Change code below this line */ }
+    return <p>{props.tasks.join(', ')}</p>
+    { /* Change code above this line */ }
+};
+
+class ToDo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>To Do Lists</h1>
+                <h2>Today</h2>
+                <List tasks={["Walk", "Cook", "Bake"]} />
+                <h2>Tomorrow</h2>
+                <List tasks={["Study", "Code", "Eat"]} />
+            </div>
+        );
+    }
+}
+
+// Use Default Props
+
+const ShoppingCart = (props) => {
+    return (
+        <div>
+            <h1>Shopping Cart Component</h1>
+        </div>
+    )
+};
+ShoppingCart.defaultProps = { items: 0 }
