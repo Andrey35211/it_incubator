5ba8e2d9f2b63f4517038ee63244bbdd320d3176
Andrey35211 <serand3521@yandex.ru>
2023-06-14 22:13:22 +0700

    codeWar 6kuy: 1 tasks

--- src/codeWars/6kyu.js
+++ src/codeWars/6kyu.js
@@ -89,3 +89,19 @@ function meeting(x, need){
     }
     return rest <= 0 ? r : 'Not enough!'
 }
+
+// Pair of gloves
+
+function numberOfPairs(gloves)
+{
+    const hash = {}
+    for(let i = 0; i < gloves.length; i++){
+        if(!hash[gloves[i]]) hash[gloves[i]] = 1;
+        else (hash[gloves[i]] = hash[gloves[i]] + 1)
+    }
+    let res = 0;
+
+    Object.keys(hash).forEach(el => res += Math.floor(hash[el] / 2))
+
+    return res
+}
