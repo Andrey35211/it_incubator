c276b4f4fff975ce2a5919ea49fa431a4c2afd4a
Andrey35211 <serand3521@yandex.ru>
2023-06-15 13:18:18 +0700

    codeWar 6kyu: 1 tasks

--- src/codeWars/6kyu.js
+++ src/codeWars/6kyu.js
@@ -105,3 +105,17 @@ function numberOfPairs(gloves)
 
     return res
 }
+
+// Are they the "same"?
+
+function comp(array1, array2){
+    if(array1 === null || array2 === null) return false
+
+    for(let i = 0; i < array2.length; i++){
+        const idxA1 = array1.indexOf(Math.sqrt(array2[i]))
+        if(idxA1 === -1) return false;
+        array1[idxA1] = null
+    }
+
+    return true
+}
