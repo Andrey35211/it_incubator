00a9cec0d56a482b57ecb69187408be8ce01c4e2
Andrey35211 <serand3521@yandex.ru>
2023-06-16 18:26:20 +0700

    codeWar 7kyu: Thinkful - Object Drills: Quarks

--- src/codeWars/kyu7.js
+++ src/codeWars/kyu7.js
@@ -217,3 +217,21 @@ function heavyMetalUmlauts(boringText) {
 // First-Class Function Factory
 
 const factory = x => arr => arr.map(el => el * x)
+
+// Thinkful - Object Drills: Quarks
+
+class Quark{
+    constructor(color, flavor){
+        this.color = color;
+        this.flavor = flavor;
+        this.baryon_number = 1 / 3;
+    }
+    interact(quark){
+        const quarkColor = quark.color;
+        quark.setColor(this.color);
+        this.color = quarkColor;
+    }
+    setColor(color){
+        this.color = color
+    }
+}
