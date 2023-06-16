714a6818a66bc8f581a7d21bc41be61e2aae79e5
Andrey35211 <serand3521@yandex.ru>
2023-06-16 18:28:33 +0700

    freeCodeCamp React: 22/47

--- src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
+++ src/freeCodeCamp/FrontEndDevelopmentLibraries/React.js
@@ -394,4 +394,23 @@ class StatefulComponent extends React.Component {
     }
 };
 
+// Render State in the User Interface
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'freeCodeCamp'
+        }
+    }
+    render() {
+        return (
+            <div>
+                { /* change code below this line */ }
+                <h1>{this.state.name}</h1>
+                { /* change code above this line */ }
+            </div>
+        );
+    }
+};
 
